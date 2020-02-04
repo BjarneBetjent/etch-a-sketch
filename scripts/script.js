9145117be2367be0af645743b046ce436b8d542d
Olav <osmevoll@gmail.com>
2020-02-04 14:21:13 +0700

    Input field for changing size

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,10 @@
-function drawBoard(numberOfDivs)
+let numberOfDivs; 
+let blackDivs = true;
+
+function drawBoard()
 {
+    if(typeof numberOfDivs === "undefined") numberOfDivs = 16;
+
     let divSize = (720 / numberOfDivs) + "px";
     for(let i = 0; i < numberOfDivs; i++)
     {
@@ -10,7 +15,8 @@ function drawBoard(numberOfDivs)
             {
                 hovering(e);
             });
-            div.classList.add("sketchHorizontalDiv");
+            div.classList.add("sketchBorderDiv");
+            div.style.backgroundColor = "white";
             div.style.height = divSize; 
             div.style.width = divSize;
             drawingBoard.appendChild(div);  
@@ -19,15 +25,42 @@ function drawBoard(numberOfDivs)
 }
 
 function hovering(e)
+{ 
+    if(blackDivs) e.target.style.backgroundColor = "black";
+    else
+    {
+        if(e.target.style.backgroundColor == "white") e.target.style.backgroundColor = random_rgba();
+    }    
+    
+}
+
+function random_rgba() 
 {
-    e.target.style.backgroundColor = "black";
+    var o = Math.round, r = Math.random, s = 255;
+    return 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + r().toFixed(1) + ')';
 }
 
 function changeSize()
 {
+    if(inputSize.value < 5) numberOfDivs = 5;
+    else if(inputSize.value > 100) numberOfDivs = 100;
+    else numberOfDivs = inputSize.value;
+
+    removeDivsFromCanvas();
+    drawBoard();
     console.log("Enter clicked")
 
 }
+
+function removeDivsFromCanvas()
+{
+    while (drawingBoard.firstChild) 
+    {
+        drawingBoard.removeChild(drawingBoard.firstChild);
+      
+    }
+}
+
 
 let border = "5px solid black"
 const drawingBoard = document.querySelector("#sketch");
@@ -36,10 +69,11 @@ let height = drawingBoard.style.height;
 console.log(drawingBoard.style.border);
 
 const inputSize = document.querySelector("#inputSize");
-inputSize.addEventListener("keyup", function(e) {
+inputSize.addEventListener("keyup", function(e) 
+{
     if(e.keyCode === 13) changeSize();
 });
 
 
-drawBoard(100);
+drawBoard();
 
